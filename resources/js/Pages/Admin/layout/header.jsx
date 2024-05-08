33a640341289f4f178c600c9d23f554a30117a61
Abid Raza <ari.abid@gmail.com>
2024-05-08 16:39:07 +0530

    Add logo and favicon

--- resources/js/Pages/Admin/layout/header.jsx
+++ resources/js/Pages/Admin/layout/header.jsx
@@ -13,13 +13,13 @@ export default function Header() {
         <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
             <nav className="h-14 flex items-center justify-between px-4">
                 <div className="hidden lg:block">
-                    <a href="/dashboard" className="flex gap-x-2 items-center">
+                    <a href="/dashboard" className="w-52 h-12 block">
                         <img
-                            src="/fav/logo-icon.svg"
+                            src="/images/logo/blaze-high-resolution-logo-transparent.png"
                             alt={appName}
-                            className="w-12 brightness-0 invert-0 dark:brightness-100"
+                            className="h-full dark:brightness-100 m-auto"
                         />
-                        <p className="text-2xl dark:text-white">{appName}</p>
+                        {/* <p className="text-2xl dark:text-white">{appName}</p> */}
                     </a>
                 </div>
                 <div className={cn("block lg:!hidden")}>
